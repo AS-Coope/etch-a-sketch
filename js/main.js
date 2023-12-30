da4739b2db0e2c5bfae146b2add31a2d572db19d
ZrC <94488901+AS-Coope@users.noreply.github.com>
2023-12-30 18:00:44 -0500

    Create 16x16 grid

--- js/main.js
+++ js/main.js
@@ -1,1 +1,18 @@
 //add 16 divs to the container
+const container = document.querySelector(".container");
+const MAX_GRID_SIZE = 16;
+const MAX_GRID_AREA = MAX_GRID_SIZE * MAX_GRID_SIZE;
+console.log(MAX_GRID_AREA);
+function createGrid(){
+  for(let numOfGrids = 0; numOfGrids < MAX_GRID_AREA; numOfGrids++){
+    let newDiv = document.createElement("div");
+    newDiv.setAttribute("class", "square");
+    addDivToContainer(newDiv);
+  }
+}
+
+function addDivToContainer(div){
+  container.append(div);
+}
+
+createGrid();
