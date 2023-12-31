7b98a92774998178eb1c61a12407daacb7c97c98
ZrC <94488901+AS-Coope@users.noreply.github.com>
2023-12-30 20:24:39 -0500

    Apply hover effect functionality

--- js/main.js
+++ js/main.js
@@ -1,18 +1,39 @@
 //add 16 divs to the container
 const container = document.querySelector(".container");
+let gridItems;
 const MAX_GRID_SIZE = 16;
 const MAX_GRID_AREA = MAX_GRID_SIZE * MAX_GRID_SIZE;
 console.log(MAX_GRID_AREA);
+
 function createGrid(){
   for(let numOfGrids = 0; numOfGrids < MAX_GRID_AREA; numOfGrids++){
     let newDiv = document.createElement("div");
     newDiv.setAttribute("class", "square");
     addDivToContainer(newDiv);
   }
+  gridItems = document.querySelectorAll(".square");
+  applyHoverEffect(gridItems, "mouseover");
+  removeHoverEffect(gridItems, "mouseout");
 }
 
 function addDivToContainer(div){
   container.append(div);
 }
 
+
+function applyHoverEffect(gridItems, event){
+  gridItems.forEach( (element) => {
+    element.addEventListener(event, ()=>{
+      element.classList.add("square-hover");
+    });
+  });
+}
+
+function removeHoverEffect(gridItems, event){
+  gridItems.forEach( (element) => {
+    element.addEventListener(event, ()=>{
+      element.classList.remove("square-hover");
+    });
+  });
+}
 createGrid();
